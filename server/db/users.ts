25b701feffdd3ed75bb0069193177a0b43308467
Matt <mattlocktom@gmail.com>
2025-11-28 18:08:36 +1300

    feat(profile): Implement profile page with authId refactor

--- server/db/users.ts
+++ server/db/users.ts
@@ -1,5 +1,6 @@
 import db from './connection.js'
 import { User, UserData } from '../../models/user.js'
+import { Post } from '../../models/post.js'
 
 // export async function getAllFruits() {
 //   const fruit = await db('fruit').select()
@@ -28,44 +29,61 @@ export async function addUser({
   return result
 }
 
+// Note - Following code has been refactored to use authId instead of numeric ID:
+
 // Get basic User info for profiles
-export async function getUserProfile(id: number): Promise<User> {
+export async function getUserProfile(authId: string): Promise<User> {
   const user = await db('users')
-    .select('id', 'name', 'bio', 'profile_picture as profilePicture')
-    .where({ id })
+    .select('id', 'name', 'bio', 'profile_picture')
+    .where('auth_id', authId)
     .first()
   return user
 }
 
 // Retrieve all posts from User
-export async function getUserPosts(id: number): Promise<Post[]> {
-  const posts = await db('posts')
+export async function getUserPosts(authId: string): Promise<Post[]> {
+  const postsFromDb = await db('posts')
+    .join('users', 'posts.user_id', 'users.id')
     .select(
-      'id',
-      'user_id as userId',
-      'message',
-      'image',
-      'date_added as dateAdded',
+      'posts.id',
+      'posts.user_id as userId',
+      'users.name as userName',
+      'posts.message',
+      'posts.image as imageUrl',
+      'posts.date_added as dateAdded', // A* Fetch the date as a string
     )
-    .where('user_id', id)
-    .orderBy('date_added', 'desc')
+    .where('users.auth_id', authId)
+    .orderBy('posts.date_added', 'desc')
+
+  // A* Map over the results and convert the date string to a UNIX timestamp in JavaScript
+  const posts = postsFromDb.map((post) => {
+    return {
+      ...post,
+      dateAdded: new Date(post.dateAdded).getTime() / 1000, // Convert to UNIX timestamp
+    }
+  })
+
   return posts
 }
 
 // Get followers of User
-export async function getFollowers(userId: number): Promise<User[]> {
+export async function getFollowers(authId: string): Promise<User[]> {
+  const userIdQuery = db('users').select('id').where('auth_id', authId)
+
   const followers = await db('followers')
     .join('users', 'followers.follower_id', 'users.id')
-    .where('followers.following_id', userId)
-    .select('users.id', 'users.name', 'users.profile_picture as profilePicture')
+    .where('followers.following_id', userIdQuery)
+    .select('users.id', 'users.name', 'users.profile_picture')
   return followers
 }
 
 // Get who User is following
-export async function getFollowing(userId: number): Promise<User[]> {
+export async function getFollowing(authId: string): Promise<User[]> {
+  const userIdQuery = db('users').select('id').where('auth_id', authId)
+
   const following = await db('followers')
     .join('users', 'followers.following_id', 'users.id')
-    .where('followers.follower_id', userId)
-    .select('users.id', 'users.name', 'users.profile_picture as profilePicture')
+    .where('followers.follower_id', userIdQuery)
+    .select('users.id', 'users.name', 'users.profile_picture')
   return following
 }
